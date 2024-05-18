fd35b8bd99828fc22a6ed30c51e794329e2b8990
dylancodeverse <ratianarivodylan@gmail.com>
2024-05-18 17:53:27 +0300

    submission

---
+++ src/components/analytics/Analytics.tsx
@@ -0,0 +1,11 @@
+import { useIonViewDidEnter } from '@ionic/react';
+import im from '../assignationForm/imgs/Analysis-pana.svg'
+import { useEffect } from 'react';
+
+const Analytics: React.FC = () => {
+    return (
+    <>
+    </>
+    )
+};
+export default Analytics;
--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,11 +1,12 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import './Tab3.css';
+import Analytics from '../components/analytics/Analytics';
 
 const Tab3: React.FC = () => {
   return (
     <IonPage>
       <IonContent fullscreen>
-        
+        <Analytics/>
       </IonContent>
     </IonPage>
   );
